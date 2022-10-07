c0d94aae1115c1570299608165b26fb65d8326a1
Kirpal <makanga.kirpal@gmail.com>
2022-10-07 18:06:05 +0200

    loadFonts: removed error spread

--- src/lib/loadFonts.js
+++ src/lib/loadFonts.js
@@ -1,7 +1,7 @@
 export default async (urls) => {
-  try {
-    return await Promise.all(urls.map(fetch))
-  } catch (e) {
-    console.error(...e)
-  }
-}
+    try {
+        return await Promise.all(urls.map(fetch));
+    } catch (e) {
+        console.error(e);
+    }
+};
