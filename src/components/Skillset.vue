eeb27f534a6ab7c5ca9597447c2bb1f140e2a550
Kirpal <makanga.kirpal@gmail.com>
2024-04-04 05:34:41 +0200

    added Skillset component

---
+++ src/components/Skillset.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const skillSet = [
+    {
+        title: '',
+        technologies: [
+            {
+                name: '',
+                proficiency: 5,
+            },
+        ],
+    },
+];
+</script>
+
+<template>
+    <section class="min-h-[100vh]"></section>
+</template>
