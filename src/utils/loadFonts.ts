639542cbc580e0986189dc5b104abc873fd84066
Kirpal <makanga.kirpal@gmail.com>
2024-04-05 01:10:44 +0200

    removed unused libs

--- src/utils/loadFonts.ts
+++
@@ -1,7 +0,0 @@
-export default async (fonts: string[]) => {
-    try {
-        return await Promise.all(fonts.map((f) => fetch(`/fonts/${f}`)));
-    } catch (e) {
-        console.error(e);
-    }
-};
