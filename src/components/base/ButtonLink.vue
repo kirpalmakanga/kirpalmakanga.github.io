7409cdb61ef60b16b1e0ed0d80d5e05a0da18892
Kirpal <makanga.kirpal@gmail.com>
2024-04-26 12:21:52 +0200

    ButtonLink: explicit colors

--- src/components/base/ButtonLink.vue
+++ src/components/base/ButtonLink.vue
@@ -1,6 +1,6 @@
 <template>
     <a
-        class="border-2 border-primary-100 px-4 py-1 rounded uppercase tracking-wide transition hover:bg-primary-100 hover:hover:text-primary-950 active:scale-110"
+        class="text-primary-100 border-2 border-primary-100 px-4 py-1 rounded uppercase tracking-wide transition hover:bg-primary-100 hover:text-gray-900 active:scale-110"
     >
         <slot />
     </a>
